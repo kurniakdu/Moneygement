e84b8b111bc30e2310dcb5d69be7fc701abdaf46
Luthfi Izzuddin <78679594+luthfi-izzuddin@users.noreply.github.com>
2022-10-31 14:29:08 +0700

    feat(ui): add login page

---
+++ Moneygement/LoginPage.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Moneygement
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.pnlSide = new System.Windows.Forms.Panel();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnForgotPw = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pnlPassword = new System.Windows.Forms.Panel();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.pbPassword = new System.Windows.Forms.PictureBox();
+            this.pnlUsername = new System.Windows.Forms.Panel();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.pbUsername = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlSide.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.pnlMain.SuspendLayout();
+            this.pnlPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPassword)).BeginInit();
+            this.pnlUsername.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsername)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSide
+            // 
+            this.pnlSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(190)))), ((int)(((byte)(203)))));
+            this.pnlSide.Controls.Add(this.pbLogo);
+            this.pnlSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSide.Location = new System.Drawing.Point(0, 0);
+            this.pnlSide.Name = "pnlSide";
+            this.pnlSide.Size = new System.Drawing.Size(344, 600);
+            this.pnlSide.TabIndex = 0;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(23, 283);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(299, 72);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogo.TabIndex = 0;
+            this.pbLogo.TabStop = false;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(233)))), ((int)(((byte)(246)))));
+            this.pnlMain.Controls.Add(this.btnSignUp);
+            this.pnlMain.Controls.Add(this.btnForgotPw);
+            this.pnlMain.Controls.Add(this.btnLogin);
+            this.pnlMain.Controls.Add(this.pnlPassword);
+            this.pnlMain.Controls.Add(this.pnlUsername);
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Controls.Add(this.lblWelcome);
+            this.pnlMain.Controls.Add(this.btnMinimize);
+            this.pnlMain.Controls.Add(this.btnClose);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(344, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(656, 600);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(233)))), ((int)(((byte)(246)))));
+            this.btnSignUp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSignUp.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(190)))), ((int)(((byte)(203)))));
+            this.btnSignUp.FlatAppearance.BorderSize = 0;
+            this.btnSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignUp.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSignUp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(73)))));
+            this.btnSignUp.Location = new System.Drawing.Point(199, 462);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(277, 24);
+            this.btnSignUp.TabIndex = 8;
+            this.btnSignUp.Text = "Didn\'t have account already?";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            // 
+            // btnForgotPw
+            // 
+            this.btnForgotPw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(233)))), ((int)(((byte)(246)))));
+            this.btnForgotPw.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnForgotPw.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(190)))), ((int)(((byte)(203)))));
+            this.btnForgotPw.FlatAppearance.BorderSize = 0;
+            this.btnForgotPw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgotPw.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnForgotPw.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(73)))));
+            this.btnForgotPw.Location = new System.Drawing.Point(199, 439);
+            this.btnForgotPw.Name = "btnForgotPw";
+            this.btnForgotPw.Size = new System.Drawing.Size(277, 26);
+            this.btnForgotPw.TabIndex = 7;
+            this.btnForgotPw.Text = "Forgot Password?";
+            this.btnForgotPw.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(190)))), ((int)(((byte)(203)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(190)))), ((int)(((byte)(203)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(199, 386);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(277, 35);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // pnlPassword
+            // 
+            this.pnlPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPassword.Controls.Add(this.tbPassword);
+            this.pnlPassword.Controls.Add(this.pbPassword);
+            this.pnlPassword.Location = new System.Drawing.Point(199, 327);
+            this.pnlPassword.Name = "pnlPassword";
+            this.pnlPassword.Size = new System.Drawing.Size(277, 42);
+            this.pnlPassword.TabIndex = 5;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(233)))), ((int)(((byte)(246)))));
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbPassword.Location = new System.Drawing.Point(41, 11);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PlaceholderText = "Password";
+            this.tbPassword.Size = new System.Drawing.Size(222, 18);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // pbPassword
+            // 
+            this.pbPassword.Image = ((System.Drawing.Image)(resources.GetObject("pbPassword.Image")));
+            this.pbPassword.Location = new System.Drawing.Point(5, 6);
+            this.pbPassword.Name = "pbPassword";
+            this.pbPassword.Size = new System.Drawing.Size(30, 30);
+            this.pbPassword.TabIndex = 1;
+            this.pbPassword.TabStop = false;
+            this.pbPassword.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbPassword_MouseDown);
+            this.pbPassword.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbPassword_MouseUp);
+            // 
+            // pnlUsername
+            // 
+            this.pnlUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlUsername.Controls.Add(this.tbUsername);
+            this.pnlUsername.Controls.Add(this.pbUsername);
+            this.pnlUsername.Location = new System.Drawing.Point(199, 279);
+            this.pnlUsername.Name = "pnlUsername";
+            this.pnlUsername.Size = new System.Drawing.Size(277, 42);
+            this.pnlUsername.TabIndex = 4;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(233)))), ((int)(((byte)(246)))));
+            this.tbUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUsername.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbUsername.Location = new System.Drawing.Point(41, 11);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.PlaceholderText = "Username";
+            this.tbUsername.Size = new System.Drawing.Size(222, 18);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // pbUsername
+            // 
+            this.pbUsername.Image = ((System.Drawing.Image)(resources.GetObject("pbUsername.Image")));
+            this.pbUsername.InitialImage = null;
+            this.pbUsername.Location = new System.Drawing.Point(5, 6);
+            this.pbUsername.Name = "pbUsername";
+            this.pbUsername.Size = new System.Drawing.Size(30, 30);
+            this.pbUsername.TabIndex = 0;
+            this.pbUsername.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(73)))));
+            this.label1.Location = new System.Drawing.Point(225, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Log in to your account";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Poppins SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblWelcome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(73)))));
+            this.lblWelcome.Location = new System.Drawing.Point(168, 168);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(349, 42);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Welcome to Moneygement";
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimize.Image")));
+            this.btnMinimize.Location = new System.Drawing.Point(572, 12);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(30, 30);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(614, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // LoginPage
+            // 
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.pnlSide.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.pnlPassword.ResumeLayout(false);
+            this.pnlPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPassword)).EndInit();
+            this.pnlUsername.ResumeLayout(false);
+            this.pnlUsername.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsername)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Panel pnlSide;
+        private PictureBox pbLogo;
+        private Panel pnlMain;
+        private Button btnMinimize;
+        private Button btnClose;
+        private Label lblWelcome;
+        private Label label1;
+        private Panel pnlPassword;
+        private PictureBox pbPassword;
+        private Panel pnlUsername;
+        private PictureBox pbUsername;
+        private TextBox tbPassword;
+        private TextBox tbUsername;
+        private Button btnLogin;
+        private Button btnForgotPw;
+        private Button btnSignUp;
+    }
+    #endregion
+}
---
+++ Moneygement/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moneygement
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pbPassword_MouseDown(object sender, MouseEventArgs e)
+        {
+            tbPassword.UseSystemPasswordChar = false;
+        }
+
+        private void pbPassword_MouseUp(object sender, MouseEventArgs e)
+        {
+            tbPassword.UseSystemPasswordChar = true;
+        }
+    }
+}
